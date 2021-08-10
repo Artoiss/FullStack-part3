8a7d401c3f93ceaf45222cfebcd57b34c2f243e2
Artoiss <aarttu@windowslive.com>
2021-08-10 21:02:31 +0300

    uibuild

---
+++ build/precache-manifest.9dfeaaf1d51edbac510d4f457dbd7c3e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a9ea7c5b37c126dbede669748bb6388a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c9e70d2edbae8f699dc1",
+    "url": "/static/css/main.34a3517d.chunk.css"
+  },
+  {
+    "revision": "cbfb2a23454fe54a500d",
+    "url": "/static/js/2.08a533a6.chunk.js"
+  },
+  {
+    "revision": "c9e70d2edbae8f699dc1",
+    "url": "/static/js/main.14fec129.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
